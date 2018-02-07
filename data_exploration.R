e359c0e19d7da069b5f1cde932b8b211f31a5d68
zuhra <azuhrawardiyah@gmail.com>
2018-02-07 15:40:28 +0700

    Initiate data_exploration script

---
+++ data_exploration.R
@@ -0,0 +1,19 @@
+# Data Exploration
+
+# load the data from SQL Server
+
+# Create connection string
+connStr <- "Driver={SQL Server};
+Server=localhost;
+Database=NYCTaxi;
+Uid=sa;
+Pwd=Passcode123"
+
+# Define the query
+sqlquery <- 'SELECT *
+FROM [NYCTaxi].[dbo].[nyctaxi]'
+
+
+
+
+
